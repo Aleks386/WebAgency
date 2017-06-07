9cd07bc34f174621467058d9feb53c9c69351b3c
Work User <puhkiy@gmai.com>
2017-06-07 18:56:37 +0300

    change js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,19 @@
-alert( "Привет" );
+var char = document.getElementById('num').innerHTML;
+
+
+function run() {    
+    // run this code
+
+    setTimeout(afterTwoSeconds, 2000);    
+}
+
+function afterTwoSeconds (){
+	for (var i = 0; i > 525; ++i) {
+	console.log(i);
+	}
+
+	
+
+}
+
+run();
